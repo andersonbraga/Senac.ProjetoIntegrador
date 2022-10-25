93c9d2e3a23a3219f92624d716112276307f045a
Anderson Braga <anderson.braga@uniplaclages.edu.br>
2022-10-25 17:30:26 -0300

    Projeto 2 Controle Estoque CRUD com Blazor WebAssembly

--- Senac.ProjetoIntegrador/ControleDeEstoque/Client/Program.cs
+++ Senac.ProjetoIntegrador/ControleDeEstoque/Client/Program.cs
@@ -1,11 +1,13 @@
 using ControleDeEstoque.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddSyncfusionBlazor(configure: options => options.IgnoreScriptIsolation = true);
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
